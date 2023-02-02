21a3d2fb88973416936244469a29d9a34e1551fa
Jorge Flores <flores.jorgeluis@gmail.com>
2023-02-02 17:24:44 -0600

    f

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 app.use(session({
     secret: 'myscret',
